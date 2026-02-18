6cd4df78294ef081c0481c5d7a79a3087f2f9107
Tuhin sarkar <tuhin.sarkar874@gmail.com>
2026-02-18 16:31:26 +0530

    Delete Cars directory

--- Cars/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('Cars', views.Cars, name='Cars'),
-    path('ExploreCars', views.ExploreCars, name='ExploreCars'),
-    path('Car/<int:id>', views.Car_Detail_View, name='Car_Detail_View'),
-    
-]
