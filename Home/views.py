8737516ca640093ee8c0773fae3782d031c7b698
Tuhin sarkar <tuhin.sarkar874@gmail.com>
2026-02-18 16:31:54 +0530

    Delete Home directory

--- Home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def Home(request):
-   #return HttpResponse('This is the home page.')
-    return render(request, 'Home.html', {} )
